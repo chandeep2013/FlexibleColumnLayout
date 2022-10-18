2b7c04f82e5b1bc3a7b67982bc273b470a58377c
chandeep2013 <37897145+chandeep2013@users.noreply.github.com>
2022-10-18 11:57:58 +0530

    Delete z_mhirj_dms_cp/z_mhirj_dms_cp directory

--- z_mhirj_dms_cp/z_mhirj_dms_cp/webapp/util/Formatter.js
+++
@@ -1,9 +0,0 @@
-sap.ui.define([
-], function () {
-	"use strict";
-	return {
-        getSwitchState : function (status){
-            return (status === undefined) ? false : status;
-        }
-    }
-})
